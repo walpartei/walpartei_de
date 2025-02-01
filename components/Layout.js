fa5f21223670ff000a4228c1399de552b3fb3bbb
Jeremias <grenzebach@pm.me>
2025-02-01 22:22:54 +0200

    Fix TypeScript config and update color scheme

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children }) {
                         href={item.href}
                         className={clsx(
                           item.current
-                            ? 'border-primary-500 text-gray-900'
+                            ? 'border-primary-500 text-primary-600'
                             : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                           'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
                         )}
