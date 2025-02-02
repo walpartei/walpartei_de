a4568dde55d8743dac0491ae2fba774ea088f4d9
Jeremias <grenzebach@pm.me>
2025-02-02 08:18:31 +0200

    Add whale emoji favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Walpartei</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
